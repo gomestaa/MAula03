4823342f82d32e64c021e5f689df6f5911691261
gomestaa <gomes.ta@hotmail.com>
2022-08-08 13:08:50 -0300

    first commit

--- src/one/digitalinnovation/basecamp/Quadrilatero.java
+++ src/one/digitalinnovation/basecamp/Quadrilatero.java
@@ -20,14 +20,4 @@ public class Quadrilatero {
         return ((baseMaior + baseMenor) * altura) / 2;
     }
 
-    public static void xpto() {
-
-        System.out.println("Antes");
-    }
-
-    public static double abc() {
-
-        return 1.6;
-    }
-
 }
